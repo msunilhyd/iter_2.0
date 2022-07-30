432ff9009f3928c4990447062528b2ff7c55b706
sunil <msunilhyd@gmail.com>
2022-07-30 17:35:07 -0500

    Generic Commit

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -5,28 +5,49 @@ import Sidebar from "./components/sidebar/Sidebar"
 import { Container } from "react-bootstrap"
 import HomeScreen from './screens/homeScreen/HomeScreen'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import "./_app.scss"
 
-const App = () => {
+const Layout = (children) => {
 
     const [sidebar, toggleSidebar] = useState(false);
 
     const handleToggleSidebar = () => toggleSidebar(value => !value);
+    return (
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar}  />
+            <div className="app__container border border-info">
+                <Sidebar 
+                    sidebar={sidebar} 
+                     handleToggleSidebar={handleToggleSidebar} 
+                />
+                <Container fluid className="app_main border border-warning">
+                    {children}
+                </Container>
+            </div>
+        </>
+    );
+}
+
+const App = () => {
 
     return (
-        //<>
-            //<Header handleToggleSidebar={handleToggleSidebar}  />
-            //<div className="app__container border border-info">
-                //<Sidebar 
-                    //sidebar={sidebar} 
-                     //handleToggleSidebar={handleToggleSidebar} 
-                ///>
-                //<Container fluid className="app_main border border-warning">
-                    //<HomeScreen />
-                //</Container>
-            //</div>
-        //</>
-        <LoginScreen />
+        <Router>
+            <Route path="/">
+                <Layout>
+                    <HomeScreen />
+                </Layout>
+            </Route>
+            <Route path='/auth'>
+                <LoginScreen />
+            </Route>
+            <Route path='/search'>
+                <Layout>
+                    <h1>Search Results</h1>
+                </Layout>
+            </Route>
+        </Router>
     )
 }
 
