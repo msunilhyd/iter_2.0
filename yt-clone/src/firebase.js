e533a59aed191464191d36c17873d59ece367047
sunil <msunilhyd@gmail.com>
2022-08-03 12:10:42 -0500

    commit

---
+++ yt-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCanFxcDYoXiBh52ESqPGKBEsZVi79r_vY",
+  authDomain: "clone-3-51de2.firebaseapp.com",
+  projectId: "clone-3-51de2",
+  storageBucket: "clone-3-51de2.appspot.com",
+  messagingSenderId: "134990465788",
+  appId: "1:134990465788:web:17031b4d6782b3b4f3e579"
+};
