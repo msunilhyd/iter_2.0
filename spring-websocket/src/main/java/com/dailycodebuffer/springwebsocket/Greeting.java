88acde8502fd778fc48ac4b77b2b17a29557bbd5
sunil <msunilhyd@gmail.com>
2022-08-18 18:28:34 -0500

    Generic Commit

---
+++ spring-websocket/src/main/java/com/dailycodebuffer/springwebsocket/Greeting.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private String message;
+}
