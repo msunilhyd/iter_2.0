c962f38c04e184d9b269d4268b2df78bf0f5ce6c
sunil <msunilhyd@gmail.com>
2022-08-26 17:24:45 -0500

    Generic Commit

--- Spring-boot-tutorial-2/src/main/java/com/dailycodebuffer/Springboottutorial2/controller/HelloController.java
+++ Spring-boot-tutorial-2/src/main/java/com/dailycodebuffer/Springboottutorial2/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.dailycodebuffer.Springboottutorial2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to DailyCodeBuffer sunil kumar";
+        return welcomeMessage;
     }
 }
