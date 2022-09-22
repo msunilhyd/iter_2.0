82993a6cc0f9c5544077be1489edd9886dc988be
sunil <msunilhyd@gmail.com>
2022-09-21 17:04:41 -0700

    Generic Commit

---
+++ leetcode/src/main/java/com/curatedList/leetcode/ContainsDuplicate/Soln.java
@@ -0,0 +1,19 @@
+package com.curatedList.leetcode.ContainsDuplicate;
+
+import java.util.HashSet;
+
+public class Soln {
+
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return false;
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i: nums) {
+            if (!set.add(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
